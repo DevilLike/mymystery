ff0fe93d08d5bf65739ee34c24fe76a7096d8a33
Kirill Tarnava <k.tarnava@itima.ru>
2012-11-11 23:05:53 +0400

    hot fix

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ActiveRecord::Base
   attr_accessible :body, :title
-  has_many :idea_requests, :dependent => :destroy
-  has_many :request, :through => :idea_requests, :dependent => :destroy
+  has_many :idea_requests
+  has_many :request, :through => :idea_requests
 end
